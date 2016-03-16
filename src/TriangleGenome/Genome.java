74afb41fa28b8b66620864b729f6e9a1df7cf2e0
CS-Labs <cseely@unm.edu>
2016-03-16 01:49:14 -0600

    Adding shell class to represent a Genome (DNA).

---
+++ src/TriangleGenome/Genome.java
@@ -0,0 +1,16 @@
+package TriangleGenome;
+
+/**
+ * This class represents a genome (DNA).
+ * The genome contains 200 triangles and
+ * each triangle contains 10 genes for a total
+ * of 2000 genes making up a genome. Since the
+ * genome represents a combination of triangles
+ * (and thus genes) it can be compared to the
+ * original image implying each Genome has a fitness.
+ * 
+ *
+ */
+public class Genome {
+
+}
