492c0f9d9b3486240887af0c7ebf4cb42a744b4b
Jesus Lopez <jlopez92@unm.edu>
2016-03-24 19:50:16 -0600

    Adds more structure to the TriangleMutation class
    TriangleMutation is now instantiated with an added integer, its
    stepValue. This value is what we will add or subtract from the gene
    value. The GA will be responsible for telling the TriangleMutation what
    to alter itself by every time.
    
    Also, mutating an RGBA value is more sophisticated. TriangleMutation now
    doesn't try to increase or decrease passed 255 or 0, respectively.

--- src/TriangleGenome/TriangleMutation.java
+++ src/TriangleGenome/TriangleMutation.java
@@ -5,137 +5,302 @@ import java.util.Random;
 
 /**
  * 
- * This class handles the actual mutation of a gene in the
- * triangle.
- * The direction tells which direction the mutation is going
- * e.g increasing or decreasing in one of the gene values. Also
- * currently the step size is always 1. In the end it should not be
- * 1, we should have some system to increase the step size when
- * we determine it should. (Adaptive Hill Climbing).
+ * This class handles the actual mutation of a gene in the triangle. The
+ * direction tells which direction the mutation is going e.g increasing or
+ * decreasing in one of the gene values.
  * 
- *
+ * When creating an object of type TriangleMutation we initialize the stepValue,
+ * which tells it how much a gene will either increase or decrease by. Right now
+ * this logic only holds for single mutations. In the future, adding in multiple
+ * mutations per step could cause problems.
+ * 
+ * The GA will handle how much the stepValue increases by after improvements.
+ * 
+ * When dealing with the rgba values, we can only have numbers [0,255]. If the
+ * GA attempts to alter the number to a value less than 0 or greater than 255,
+ * the value is instead 0 or 255, respectively. If it tries to again go passed
+ * the bounds, the triangle will not mutate at all, essentially wasting a step
+ * and causes no improvement. For example, If the r value is 7 and the step
+ * value is 10 and we're decreasing, the r value will be 0, not -3. If that's an
+ * improvement and the GA wants to again decrease r, the value remains 0 and
+ * causes no improvement - So, the GA moves on.
  */
-public class TriangleMutation {
-	Random random = new Random();
-	Triangle triangle;
-	/**
-	 * 
-	 * @param The triangle that will have genes mutated. 
-	 */
-	TriangleMutation(Triangle triangle)
-	{
-		this.triangle = triangle;
-	}
-	
-	public void mutateP1X(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setP1(new Point((triangle.getP1().x+1), triangle.getP1().y));
-		}
-		else
-		{
-			triangle.setP1(new Point((triangle.getP1().x-1), triangle.getP1().y));
-		}
-	}
-	public void mutateP1Y(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setP1(new Point((triangle.getP1().x), triangle.getP1().y+1));
-		}
-		else
-		{
-			triangle.setP1(new Point((triangle.getP1().x), triangle.getP1().y-1));
-		}
-	}
-	public void mutateP2X(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setP2(new Point((triangle.getP2().x+1), triangle.getP2().y));
-		}
-		else
-		{
-			triangle.setP2(new Point((triangle.getP2().x-1), triangle.getP2().y));
-		}
-	}
-	public void mutateP2Y(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setP2(new Point((triangle.getP2().x), triangle.getP2().y+1));
-		}
-		else
-		{
-			triangle.setP2(new Point((triangle.getP2().x), triangle.getP2().y-1));
-		}
-	}
-	public void mutateP3X(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setP3(new Point((triangle.getP3().x+1), triangle.getP3().y));
-		}
-		else
-		{
-			triangle.setP3(new Point((triangle.getP3().x-1), triangle.getP3().y));
-		}
-	}
-	public void mutateP3Y(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setP3(new Point((triangle.getP3().x), triangle.getP3().y+1));
-		}
-		else
-		{
-			triangle.setP3(new Point((triangle.getP3().x), triangle.getP3().y-1));
-		}
-	}
-	public void mutateAlpha(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setAlpha(triangle.getAlpha()+1);
-		}
-		else
-		{
-			triangle.setAlpha(triangle.getAlpha()-1);
-		}
-	}
-	public void mutateRed(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setRed(triangle.getRed()+1);
-		}
-		else
-		{
-			triangle.setRed(triangle.getRed()-1);
-		}
-	}
-	public void mutateBlue(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setBlue(triangle.getBlue()+1);
-		}
-		else
-		{
-			triangle.setBlue(triangle.getBlue()-1);
-		}
-	}
-	public void mutateGreen(boolean direction)
-	{
-		if(direction)
-		{
-			triangle.setGreen(triangle.getGreen()+1);
-		}
-		else
-		{
-			triangle.setGreen(triangle.getGreen()-1);
-		}
-	}
-	
+public class TriangleMutation
+{
+  Random random = new Random();
+  Triangle triangle;
+  int stepValue;
+
+  /**
+   * 
+   * @param triangle
+   *          the triangle that will have genes mutated
+   * @param step
+   *          the value to increase or decrease a gene by
+   */
+  TriangleMutation(Triangle triangle, int step)
+  {
+    this.triangle = triangle;
+    stepValue = step;
+  }
+
+  public void mutateP1X(boolean direction)
+  {
+    if (direction)
+    {
+      triangle.setP1(new Point((triangle.getP1().x + stepValue), triangle.getP1().y));
+    }
+    else
+    {
+      triangle.setP1(new Point((triangle.getP1().x - stepValue), triangle.getP1().y));
+    }
+  }
+
+  public void mutateP1Y(boolean direction)
+  {
+    if (direction)
+    {
+      triangle.setP1(new Point((triangle.getP1().x), triangle.getP1().y + stepValue));
+    }
+    else
+    {
+      triangle.setP1(new Point((triangle.getP1().x), triangle.getP1().y - stepValue));
+    }
+  }
+
+  public void mutateP2X(boolean direction)
+  {
+    if (direction)
+    {
+      triangle.setP2(new Point((triangle.getP2().x + stepValue), triangle.getP2().y));
+    }
+    else
+    {
+      triangle.setP2(new Point((triangle.getP2().x - stepValue), triangle.getP2().y));
+    }
+  }
+
+  public void mutateP2Y(boolean direction)
+  {
+    if (direction)
+    {
+      triangle.setP2(new Point((triangle.getP2().x), triangle.getP2().y + stepValue));
+    }
+    else
+    {
+      triangle.setP2(new Point((triangle.getP2().x), triangle.getP2().y - stepValue));
+    }
+  }
+
+  public void mutateP3X(boolean direction)
+  {
+    if (direction)
+    {
+      triangle.setP3(new Point((triangle.getP3().x + stepValue), triangle.getP3().y));
+    }
+    else
+    {
+      triangle.setP3(new Point((triangle.getP3().x - stepValue), triangle.getP3().y));
+    }
+  }
+
+  public void mutateP3Y(boolean direction)
+  {
+    if (direction)
+    {
+      triangle.setP3(new Point((triangle.getP3().x), triangle.getP3().y + stepValue));
+    }
+    else
+    {
+      triangle.setP3(new Point((triangle.getP3().x), triangle.getP3().y - stepValue));
+    }
+  }
+
+  public void mutateAlpha(boolean direction)
+  {
+    int value = triangle.getAlpha();
+
+    if (isValidRGBAMutation(direction, value))
+    {
+      if (direction)
+      {
+        value += stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setAlpha(value);
+        }
+        else
+        {
+          triangle.setAlpha(255);
+        }
+      }
+      else
+      {
+        value -= stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setAlpha(value);
+        }
+        else
+        {
+          triangle.setAlpha(0);
+        }
+      }
+    }
+  }
+
+  public void mutateRed(boolean direction)
+  {
+    int value = triangle.getRed();
+
+    if (isValidRGBAMutation(direction, value))
+    {
+      if (direction)
+      {
+        value += stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setRed(value);
+        }
+        else
+        {
+          triangle.setRed(255);
+        }
+      }
+      else
+      {
+        value -= stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setRed(value);
+        }
+        else
+        {
+          triangle.setRed(0);
+        }
+      }
+    }
+  }
+
+  public void mutateBlue(boolean direction)
+  {
+    int value = triangle.getBlue();
+
+    if (isValidRGBAMutation(direction, value))
+    {
+      if (direction)
+      {
+        value += stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setBlue(value);
+        }
+        else
+        {
+          triangle.setBlue(255);
+        }
+      }
+      else
+      {
+        value -= stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setBlue(value);
+        }
+        else
+        {
+          triangle.setBlue(0);
+        }
+      }
+    }
+  }
+
+  public void mutateGreen(boolean direction)
+  {
+    int value = triangle.getGreen();
+
+    if (isValidRGBAMutation(direction, value))
+    {
+      if (direction)
+      {
+        value += stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setGreen(value);
+        }
+        else
+        {
+          triangle.setGreen(255);
+        }
+      }
+      else
+      {
+        value -= stepValue;
+        if (!isMinOrMaxRGBA(value))
+        {
+          triangle.setGreen(value);
+        }
+        else
+        {
+          triangle.setGreen(0);
+        }
+      }
+    }
+  }
+
+  /**
+   * @param value
+   *          the specific r,g,b, or a value (range 0-255) of the triangle
+   * @return true if the value is <= 0 or >= 255
+   */
+  private boolean isMinOrMaxRGBA(int value)
+  {
+    if (value <= 0 || value >= 255)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Determines if a mutation we are trying to make to an RGBA value is valid or
+   * not. There are two invalid moves, decreasing 0 or increasing 255. If the
+   * value is any number between (but not including) 0 and 255, there is no
+   * invalid mutation.
+   * 
+   * @param direction
+   *          true if increasing, false if decreasing
+   * @param value
+   *          the specific r,g,b, or a value of the triangle
+   * @return true if a valid mutation
+   */
+  private boolean isValidRGBAMutation(boolean direction, int value)
+  {
+    if (!isMinOrMaxRGBA(value))
+    {
+      // if the initial value is not a min or max we can alter the value 100%
+      return true;
+    }
+    else
+    {
+      if (value == 0 && !direction)
+      {
+        // if the initial value is 0 and we're trying to decrease, detect an
+        // illegal mutation
+        return false;
+      }
+      else if (value == 255 && direction)
+      {
+        // if the initial value is 255 and we're trying to increase, detect an
+        // illegal mutation
+        return false;
+      }
+      else
+      {
+        // we are attempting to increase a min or decrease a max, which is legal
+        return true;
+      }
+    }
+  }
+
 }
