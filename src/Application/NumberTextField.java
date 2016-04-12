0218b77cd18a60c7538cb448a7044772482570e0
Jesus Lopez <jlopez92@unm.edu>
2016-04-11 21:35:48 -0600

    Adds JavaDoc

--- src/Application/NumberTextField.java
+++ src/Application/NumberTextField.java
@@ -2,29 +2,26 @@ package Application;
 
 import javafx.scene.control.TextField;
 
-public class NumberTextField extends TextField
-{
-  public NumberTextField()
-  {
-    this.setPromptText("Enter only #s");
-  }
+/**
+ * A modification of the TextField container in JavaFX. This allows for only for
+ * numerical values to be entered into the textfield.
+ */
+public class NumberTextField extends TextField {
+	public NumberTextField() {
+		this.setPromptText("Enter only #s");
+	}
 
-  @Override
-  public void replaceText(int i, int i1, String string)
-  {
-    if (string.matches("[0-9]"))
-    {
-      super.replaceText(i, i1, string);
-    }
-    else if (string.isEmpty())
-    {
-      super.replaceText(0, 0, "0");
-    }
-  }
+	@Override
+	public void replaceText(int i, int i1, String string) {
+		if (string.matches("[0-9]")) {
+			super.replaceText(i, i1, string);
+		} else if (string.isEmpty()) {
+			super.replaceText(0, 0, "0");
+		}
+	}
 
-  @Override
-  public void replaceSelection(String string)
-  {
-    super.replaceSelection(string);
-  }
+	@Override
+	public void replaceSelection(String string) {
+		super.replaceSelection(string);
+	}
 }
